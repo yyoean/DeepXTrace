e0c57d1a7409feff9ad9b35f2e7cef4ecf8741c9
wangfakang <fakangwang@gmail.com>
2025-07-29 16:12:01 +0800

    add test case for diagnose
    Signed-off-by: wangfakang <fakangwang@gmail.com>

---
+++ tests/test_diagnose.py
@@ -0,0 +1,70 @@
+import unittest
+import numpy as np
+
+from deepxtrace import diagnose as ds
+
+
+class TestDiagnose(unittest.TestCase):
+    def setUp(self):
+        self.abnormal_row = np.array([
+        [     934,     1501, 764,       463, 526,      526,      1553,     1102],
+        [     488,      501,  517,      485, 497,      547,      484,       517],
+        [     934,     1501, 764,       463, 526,      526,      1553,     1102],
+        [19779445, 19779792, 19773172,  493, 19779409, 19779527, 535,  19779431],
+        [     488,      501, 517,       485, 497,      547,      484,       517],
+        [     684,      440, 230,       476, 197,      478,      538,       531],
+        [     488,      501, 517,       485, 497,      547,      484,       517],
+        [     488,      501, 517,       485, 497,      547,      484,       517],
+
+        ])
+        self.abnormal_col = np.array([
+        [     449,      637, 19776694,      480, 19779137,      487,      466,      411],
+        [     684,      440, 19751230,      476, 19779054,      478,      538,      531],
+        [     488,      501,      517,      485, 19779234,      547,      484,      517],
+        [    1263,     2052, 19757762,      440, 19779484,      476,     2602,     2947],
+        [     934,     1501, 19776764,      463,      526,      526,     1553,     1102],
+        [    2343,     1807, 19779054,      486, 19779895,      517,     1979,     2407],
+        [     445,      792, 19773172,      493, 19779409,      527,      535,      431],
+        [     477,      663, 19774488,      506, 19779483,      604,      555,      474],
+        ])
+
+        self.abnormal_mixed = np.array([
+        [     449, 637,      19776694,      480,  19779137,  486,      466,       411],
+        [     684, 440,      19751230,      476,  19779054,  478,      538,       531],
+        [     488, 501,      517,           485,  19779234,  947,      484,      517],
+        [    1263, 2052,     19757762,      440,  19779484,  476,      2602,     2947],
+        [     934, 1501,     19776764,      463,      526,   526,      1553,     1102],
+        [    2343, 1807,     19779054,      486,  19779895,  517,      1979,     2407],
+        [19779445, 19779792, 19773172,  19779493, 19779409,  19779527, 535,  19779431],
+        [     477, 663,      19774488,      506,  19779483,  604,      555,       474],
+        ])
+
+        self.abnormal_point = np.array([
+        [ 16, 13, 11, 17, 18, 18, 19, 12],
+        [ 10, 19, 11, 18, 16, 16, 16, 13],
+        [ 18, 18, 11, 10, 18, 19, 18, 13],
+        [ 13, 18, 11, 11, 125, 11, 18, 18],
+        [ 14, 20, 11, 14, 14, 16, 18, 16],
+        [ 20, 20, 11, 14, 19, 13, 15, 18],
+        [38,17,13,16, 13,13,13,13],
+        [ 15, 17, 12, 18, 13, 13, 15, 14],
+        ])
+
+    def test_diagnose_row(self):
+        res = ds.Diagnose.diagnose_matrix(self.abnormal_row)
+        self.assertEqual(res, {'abnormal_cols': [], 'abnormal_rows': [3], 'abnormal_points': []})
+
+    def test_diagnose_col(self):
+        res = ds.Diagnose.diagnose_matrix(self.abnormal_col)
+        self.assertEqual(res, {'abnormal_cols': [2, 4], 'abnormal_rows': [], 'abnormal_points': []})
+
+    def test_diagnose_mixed(self):
+        res = ds.Diagnose.diagnose_matrix(self.abnormal_mixed, thres_col=1.0, thres_row=1.0)
+        self.assertEqual(res, {'abnormal_cols': [2, 4], 'abnormal_rows': [6], 'abnormal_points': []})
+
+    def test_diagnose_point(self):
+        res = ds.Diagnose.diagnose_matrix(self.abnormal_point)
+        self.assertEqual(res, {'abnormal_cols': [], 'abnormal_rows': [], 'abnormal_points': [(3, 4, 125, 7.279344854723584)]})
+
+if __name__ == '__main__':
+    unittest.main()
